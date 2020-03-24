e5513e425a7bd41e79fc7e7e404f00697a71754e
John Lupton <johnlupton@JohnniMusicMac.local>
2020-03-24 22:43:26 +0000

    Prepare for heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,6 @@ const webpack = require("webpack");
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-//entry point and output location
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
-}
-
 module.exports = env => {
   console.log("env", env);
 
